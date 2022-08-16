1ad09f1fb871ff806d6f4b4c12e71d5257533a24
ahriandcat <95160129+ahriandcat@users.noreply.github.com>
2022-08-16 14:00:09 +0700

    Add files via upload

---
+++ demoEmailWithTemplate/src/main/java/com/example/demoemailwithtemplate/DemoEmailWithTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoemailwithtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEmailWithTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEmailWithTemplateApplication.class, args);
+    }
+
+}
---
+++ demoEmailWithTemplate/src/test/java/com/example/demoemailwithtemplate/DemoEmailWithTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoemailwithtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEmailWithTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
